b9ea125c732300ff2acf5eacf2b73eb9eb7080a1
MyWotersTini <mcpe0.15.4.mc@gmail.com>
2023-11-09 13:25:42 +0200

    added tables and mini update

--- manufacture_edit.php
+++ manufacture_edit.php
@@ -14,4 +14,33 @@ foreach($manufactures as $item){
 }
 ?>
 
+<div class="table_edit">
+    <div class="table_edit-container">
+        <div class="table_edit-header">
+            <div class="table_edit-header-item"> Name </div>
+            <div class="table_edit-header-item"> Country </div>
+            <div class="table_edit-header-item"> Count </div>
+        </div>
+        <div class="table_edit-content">
+            
+            <?php foreach($manufactures as $item){ ?>
+                <div class="table_edit-content-item"> 
+                    <div class="table_edit-content-item-td"> 
+                        <?php echo $item['name'] ?>
+
+                        <div class="actions_block">
+                            <!-- має з'являтися при наведення на table_edit-content-item-td -->
+
+                            <a href="">Edit</a>
+                            <a href="" class="red">Trash</a>
+                        </div>
+                    </div>
+                    <div class="table_edit-content-item-td"> <?php echo $item['country'] ?> </div>
+                    <div class="table_edit-content-item-td"> <?php echo '0' ?>  </div>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+
 <?php include "footer.php"; ?>
---
+++ system/functions.php
@@ -0,0 +1,47 @@
+<?php 
+
+/*
+
+    $args = array(
+        'type'  => 'input', // select, textarea
+        'format' => 'password',
+        'name' => 'input_field_1',
+        'placeholder' => 'Enter name'
+    );
+
+*/
+
+function get_input_field($args = []){
+    
+    if(empty($args))
+        return;
+
+    // echo 'test';
+    switch($args['type']){
+        case 'input': 
+
+            echo '<input ';
+
+            if(!empty($args['format']))
+                echo 'type="' . $args['format'] . '" ';
+            
+            if(!empty($args['name']))
+                echo 'name="' . $args['name'] . '" ';
+            
+            if(!empty($args['placeholder']))
+                echo 'placeholder="' . $args['placeholder'] . '" ';
+
+            echo '>';
+            break;
+            
+        case 'select': 
+            break;
+            
+        case 'textarea': 
+            break;
+    }
+}
+
+?>
+
+<input type="text" vlaue>
